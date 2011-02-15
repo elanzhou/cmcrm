9eedf6054776255aa10b6a2900022c958a7ea6fd
elan <elanzhou@126.com>
2011-02-16 07:44:02 +0800

    geo importer 004

--- src/com/elanzone/cmcrm/tools/geo/GeoImporter.java
+++ src/com/elanzone/cmcrm/tools/geo/GeoImporter.java
@@ -1,6 +1,7 @@
 package com.elanzone.cmcrm.tools.geo;
 
 import org.ofbiz.base.util.UtilHttp;
+import org.ofbiz.base.util.FileUtil;
 import org.ofbiz.entity.Delegator;
 import org.ofbiz.entity.GenericValue;
 import org.ofbiz.security.authz.Authorization;
@@ -8,6 +9,7 @@ import org.ofbiz.service.LocalDispatcher;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.util.Locale;
 import java.util.Map;
 import java.util.TimeZone;
@@ -22,32 +24,55 @@ public class GeoImporter {
         Locale locale = UtilHttp.getLocale(request);
         TimeZone timeZone = UtilHttp.getTimeZone(request);
 
-        Map<String, Object> params = UtilHttp.getParameterMap(request);
-        // get the schedule parameters
-//        String jobName = (String) params.remove("JOB_NAME");
+        File uploadFile = FileUtil.getFile("runtime/tmp");
+        ServletFileUpload dfu = new ServletFileUpload(new DiskFileItemFactory(10240, uploadFile));
+        java.util.List lst = null;
+        try {
+            lst = dfu.parseRequest(request);
+        } catch (FileUploadException e4) {
+            request.setAttribute("_ERROR_MESSAGE_", e4.getMessage());
+            Debug.logError("[UploadContentAndImage.uploadContentAndImage] " + e4.getMessage(), module);
+            return "error";
+        }
+        //if (Debug.infoOn()) Debug.logInfo("[UploadContentAndImage]lst " + lst, module);
 
-        // the frequency map
-//        Map<String, Integer> freqMap = FastMap.newInstance();
+        if (lst.size() == 0) {
+            String errMsg = UtilProperties.getMessage(UploadContentAndImage.err_resource, "uploadContentAndImage.no_files_uploaded", locale);
+            request.setAttribute("_ERROR_MESSAGE_", errMsg);
+            Debug.logWarning("[DataEvents.uploadImage] No files uploaded", module);
+            return "error";
+        }
 
-//        freqMap.put("SECONDLY", Integer.valueOf(1));
-//        String errMsg = UtilProperties.getMessage(CoreEvents.err_resource, "coreEvents.service_scheduled", locale);
-//        request.setAttribute("_EVENT_MESSAGE_", errMsg);
-//        if (null!=syncServiceResult) {
-//            request.getSession().setAttribute("_RUN_SYNC_RESULT_", syncServiceResult);
-//            return "sync_success";
-//        }
+        Map passedParams = FastMap.newInstance();
+        FileItem fi = null;
+        FileItem imageFi = null;
+        byte[] imageBytes = {};
+        for (int i = 0; i < lst.size(); i++) {
+            fi = (FileItem) lst.get(i);
+            //String fn = fi.getName();
+            String fieldName = fi.getFieldName();
+            if (fi.isFormField()) {
+                String fieldStr = fi.getString();
+                passedParams.put(fieldName, fieldStr);
+            } else if (fieldName.equals("imageData")) {
+                imageFi = fi;
+                imageBytes = imageFi.get();
+            }
+        }
+        if (Debug.infoOn()) Debug.logInfo("[UploadContentAndImage]passedParams: " + passedParams, module);
+
+        String countryId = passedParams.get("country");
+
+        Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+        String database = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)};DBQ=" + uploadFile.getAbsolutePath()  + ";DriverID=22;READONLY=true}";
+        Connection connection = java.sql.DriverManager.getConnection(database, "", "");
+        Statement statement = connection.createStatement();
+        String provinceSql = "select * from province";
+        ResultSet rs = statement.execute(provincSql);
+
+
+
         return "success";
     }
 
-
-    /**
-     * a
-     *
-     * @param args
-     *          --filename
-     *          --country
-     *          --format
-     */
-    public static void main(String args[]) {
-    }
 }
