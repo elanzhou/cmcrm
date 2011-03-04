037576607d5d79e0c6d48c294ba7ccb2594d4bf0
elan <elanzhou@126.com>
2011-03-04 19:14:16 +0800

    geodealer tester

---
+++ src/com/elanzone/cmcrm/test/GeoDealerTester.java
@@ -0,0 +1,13 @@
+package com.elanzone.cmcrm.test;
+
+import com.elanzone.cmcrm.tools.geo.GeoDealerAccessImp;
+import junit.framework.TestCase;
+
+
+public class GeoDealerTester extends TestCase {
+
+    public void testAnalyzeData() {
+        GeoDealerAccessImp geoDealer = new GeoDealerAccessImp(null);
+        // todo
+    }
+}
