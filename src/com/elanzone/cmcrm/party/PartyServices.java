a7c3abb9324f8b47e9efb2064a2ebb882da6977e
elan <elanzhou@126.com>
2011-07-24 21:46:20 +0800

    #2 公司与组织架构管理 : 003: 公司相关内容

--- src/com/elanzone/cmcrm/party/PartyServices.java
+++ src/com/elanzone/cmcrm/party/PartyServices.java
@@ -1,6 +1,10 @@
 package com.elanzone.cmcrm.party;
 
-import org.ofbiz.service.DispatchContext;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.service.*;
 
 import java.util.Map;
 
@@ -11,26 +15,45 @@ public class PartyServices {
     public static final String resourceError = "PartyErrorUiLabels";
 
     /**
-     * Creates a Company
+     * Create or Update the Company
      *
-     * @param ctx The DispatchContext that this service is operating in.
+     * @param dctx The DispatchContext that this service is operating in.
      * @param context Map containing the input parameters.
      * @return Map with the result of the service, the output parameters.
      */
-    public static Map<String, Object> createCompany(DispatchContext ctx, Map<String, ? extends Object> context) {
-        context.put("partyId", "COMPANY");
-        context.put("partyTypeId", "CORPORATION");
-        return org.ofbiz.party.party.PartyServices.createPartyGroup(ctx, context);
+    public static Map<String, Object> createOrUpdateCompany(DispatchContext dctx, Map<String, ? extends Object> context) {
+        Delegator delegator = dctx.getDelegator();
+        LocalDispatcher dispatcher = dctx.getDispatcher();
+        String companyPartyId = "COMPANY";
+        String partygroupServiceName;
+        try {
+            GenericValue company = delegator.findByPrimaryKey("Party", UtilMisc.toMap("partyId", companyPartyId));
+            if (company == null) {
+                partygroupServiceName = "createPartyGroup";
+            }
+            else {
+                partygroupServiceName = "updatePartyGroup";
+            }
+
+            ModelService createAccountService = dctx.getModelService(partygroupServiceName);
+            Map<String, Object> partygroupContext = createAccountService.makeValid(context, ModelService.IN_PARAM);
+            createAccountService.put("partyId", companyPartyId);
+            createAccountService.put("partyTypeId", "CORPORATION");
+
+            Map<String, Object> pgOpResult = dispatcher.runSync(partygroupServiceName, partygroupContext);
+            if (ServiceUtil.isError(pgOpResult) || ServiceUtil.isFailure(pgOpResult)) {
+                return pgOpResult;
+            }
+
+        } catch (GenericServiceException gse) {
+            return ServiceUtil.returnError(gse.getMessage());
+        } catch (GenericEntityException gee) {
+            return ServiceUtil.returnError(gee.getMessage());
+        }
+
+        Map<String, Object> result = ServiceUtil.returnSuccess();
+        result.put("partyId", companyPartyId);
+        return result;
     }
 
-    /**
-     * Updates the Company.
-     *
-     * @param ctx The DispatchContext that this service is operating in.
-     * @param context Map containing the input parameters.
-     * @return Map with the result of the service, the output parameters.
-     */
-    public static Map<String, Object> updateCompany(DispatchContext ctx, Map<String, ? extends Object> context) {
-        return org.ofbiz.party.party.PartyServices.updatePartyGroup(ctx, context);
-    }
 }
